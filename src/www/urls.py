ca0fa64ed9e7133693c50bf056b67e8801dbdbe0
kdeoliveira <contact@kevindeoliveira.com>
2020-02-28 18:20:49 -0500

    second commit for intro

--- src/www/urls.py
+++ src/www/urls.py
@@ -15,9 +15,14 @@
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.intro, name='intro'),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.STATIC_ROOT)
